b6249f1cafd511012d8f5fb1ed593f1fe44ea042
b.naskov <boni.mislyashki@gmail.com>
2014-11-22 18:59:04 +0200

    lecture 8

--- SpringFramework/spring-tutorial/src/main/java/com/bnaskov/App.java
+++ SpringFramework/spring-tutorial/src/main/java/com/bnaskov/App.java
@@ -1,18 +1,21 @@
 package com.bnaskov;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = new FileSystemXmlApplicationContext(
-				"beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/bnaskov/beans/beans.xml");
 
 		// the id: "person" is set in the beans.xml
 		Person person = (Person) context.getBean("person");
 		person.speak();
-	}
 
+		System.out.println(person);
+
+		((ClassPathXmlApplicationContext) context).close();
+	}
 }
